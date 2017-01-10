063b3b43087d3187da3c34d383664c4ff5b482c2
jerrywangalready <sunnyamos@163.com>
2017-01-10 20:41:54 +0800

    登录实现

--- src/main/java/com/sgcc/comm/interceptor/AllInterceptor.java
+++ src/main/java/com/sgcc/comm/interceptor/AllInterceptor.java
@@ -1,28 +1,68 @@
 package com.sgcc.comm.interceptor;
 
+import com.sgcc.pesticide.login.model.UserToken;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.context.request.WebRequestInterceptor;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * @author jerrywang
  * @create 2017/1/10.
  */
-public class AllInterceptor implements WebRequestInterceptor {
+public class AllInterceptor implements HandlerInterceptor {
 
 
     @Override
-    public void preHandle(WebRequest webRequest) throws Exception {
-        System.out.println(111);
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+
+        HttpSession session = httpServletRequest.getSession();
+
+        //无需登录，允许访问的地址
+        String[] allowUrls =new String[]{"/toLogin","/login"};
+
+        //获取请求地址
+        String url =httpServletRequest.getRequestURL().toString();
+
+        for (String strUrl : allowUrls) {
+            if(url.contains(strUrl))
+            {
+                return true;
+            }
+        }
+
+        //获得session中的用户
+        UserToken user =(UserToken) session.getAttribute("userToken");
+
+
+        if(user ==null)
+        {
+//            throw new UnLoginException("您尚未登录！");
+//            System.out.println(111);
+            //重定向
+            httpServletResponse.sendRedirect(httpServletRequest.getContextPath()+"/login/login.do");
+
+        }
+        //        String loginState = httpServletRequest.getSession()==null?"":httpServletRequest.getSession().getAttribute("username").toString();
+//        if("".equals(loginState)) {
+//        }else {
+//            System.out.println(222);
+//        }
+        return true;
     }
 
     @Override
-    public void postHandle(WebRequest webRequest, ModelMap modelMap) throws Exception {
-        System.out.println(222);
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
     }
 
     @Override
-    public void afterCompletion(WebRequest webRequest, Exception e) throws Exception {
-        System.out.println(333);
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
     }
 }
--- src/main/webapp/WEB-INF/pesticide/login/login.jsp
+++ src/main/webapp/WEB-INF/pesticide/login/login.jsp
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <%--
   Created by IntelliJ IDEA.
   User: jerrywang
@@ -8,9 +9,25 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
+    <%
+        String path = request.getContextPath();
+    %>
     <title>登录</title>
+    <script type="text/javascript" src="<%=path%>/comm/plugins/jQuery/jquery-3.1.1.min.js"></script>
 </head>
 <body>
-    登录
+    <input id="username" >
+    <button id="loginButton">登录</button>
 </body>
+    <script type="text/javascript">
+        var path = '<%=path%>';
+        $(function () {
+            $("#loginButton").click(function () {
+                $.post(path+"/login/doLogin.do",{},function () {
+
+                });
+            });
+        })
+
+    </script>
 </html>
