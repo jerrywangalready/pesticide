eea90f7483cf27e217f3172ead966c893501ede1
jerrywangalready <sunnyamos@163.com>
2017-01-20 22:43:09 +0800

    字典

--- src/main/java/com/sgcc/comm/dictionary/controller/DictionaryController.java
+++ src/main/java/com/sgcc/comm/dictionary/controller/DictionaryController.java
@@ -1,6 +1,8 @@
 package com.sgcc.comm.dictionary.controller;
 
 import com.sgcc.comm.dictionary.service.DictionaryService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +23,8 @@ import java.util.Map;
 @Controller
 @RequestMapping("/dict")
 public class DictionaryController {
+
+    static Logger logger = LogManager.getLogger(DictionaryController.class.getName());
 
     @Autowired
     DictionaryService dictionaryService;
