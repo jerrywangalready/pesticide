04d278b1431fbdb47d112795447999d42b06ba1b
jerrywangalready <sunnyamos@163.com>
2017-02-06 13:34:44 +0800

    富编辑器整理

--- src/main/webapp/comm/plugins/ckeditor/config.js
+++ src/main/webapp/comm/plugins/ckeditor/config.js
@@ -10,26 +10,39 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.toolbarGroups = [
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'styles' },
-		{ name: 'undo' },
-		// { name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		// { name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		// { name: 'links' },
-		// { name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'links' },
 		{ name: 'insert' },
-		{ name: 'colors' },//,
-		{ name: 'tools' },
 		{ name: 'forms' },
-		{ name: 'others' }
-		// '/',
-		//{ name: 'about' }
+		{ name: 'tools' },
+		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'others' },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		{ name: 'styles' },
+		{ name: 'colors' },
+		{ name: 'about' }
+	];
+
+
+	config.toolbar = 'MyToolBar';
+
+	config.toolbar_MyToolBar = [
+		['Undo','Redo'],
+		['Bold','Italic','Underline','Strike'],
+		['Font','Format','Styles','TextColor','FontSize','BGColor'],
+		['NumberedList','BulletedList'],
+		['Table','Image','HorizontalRule'],
+		['Form','Checkbox'],
+		['RemoveFormat'],
+		['Maximize']
 	];
 
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
-	// config.removeButtons = 'Subscript,Superscript';
+	config.removeButtons = 'Subscript,Superscript';
 
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
