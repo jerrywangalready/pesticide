791ba9f4e13f0a93575c291024d7f2f9027a1f2a
jerrywangalready <sunnyamos@163.com>
2017-05-16 23:25:42 +0800

    增加每页显示条数按钮和总条数

--- src/main/webapp/comm/plugins/page/page.js
+++ src/main/webapp/comm/plugins/page/page.js
@@ -7,7 +7,9 @@ $.fn.extend({
             var _this = $(this);
 
             _this.addClass("page-bar").attr("aria-label","Page navigation");
-            var ul = "<ul class='pagination pagination-sm'>";
+            _this.html("<ul class='pagination pagination-sm left'><li><a>10</a></li><li><a>20</a></li><li><a>50</a></li><li><a>100</a></li></ul><span class='badge    page-bar-total'>"+data.total+"</span>")
+
+            var ul = "<ul class='pagination pagination-sm right'>";
             ul += "<li><a aria-label='Previous'><span aria-hidden='true'>&laquo;</span></a></li>";
             // var size = data.total%data.pageSize == 0 && data.total != 0 ? data.total/data.pageSize : data.total/data.pageSize + 1;
             var pn = parseInt((data.total-1)/data.pageSize) + 1;
@@ -20,29 +22,37 @@ $.fn.extend({
             }
             ul += "<li><a aria-label='Next'><span aria-hidden='true'>&raquo;</span></a></li>";
             ul += "</ul>";
-            _this.html(ul);
+            _this.append(ul);
             if(data.pageNum == 1){
-                _this.find("li:first").addClass("disabled");
+                _this.find("ul:eq(1) li:first").addClass("disabled");
             }
             if(data.pageNum == pn){
-                _this.find("li:last").addClass("disabled");
+                _this.find("ul:eq(1) li:last").addClass("disabled");
             }
             // 数字按钮绑定
-            _this.find("li:not(:first,:last)").each(function () {
+            _this.find("ul:eq(1) li:not(:first,:last)").each(function () {
                $(this).click(function () {
                    comm.js.hashAppand("pn",$(this).find("a").text());
                });
             });
             // 向前按钮绑定
-            _this.find("li:first").click(function () {
+            _this.find("ul:eq(1) li:first").click(function () {
                 if(data.pageNum > 1){
                     comm.js.hashAppand("pn",data.pageNum - 1);
                 }
             });
             // 向后按钮绑定
-            _this.find("li:last").click(function () {
+            _this.find("ul:eq(1) li:last").click(function () {
                 if(data.pageNum < pn){
                     comm.js.hashAppand("pn",data.pageNum + 1);
+                }
+            });
+            // 初始化每页显示条数
+            _this.find("ul:eq(0) a").click(function () {
+                comm.js.hashAppand("ps",$(this).text());
+            }).each(function () {
+                if(data.pageSize == $(this).text()) {
+                    $(this).parent().addClass("active");
                 }
             });
         });
